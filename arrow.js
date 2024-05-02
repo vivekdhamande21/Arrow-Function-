e1cd9a3851799a78a7583dd61e55360691e02552
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-05-02 12:37:32 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,27 @@
+//Arrow Function 
+const sum = (a,b) => {
+    console.log(a+b);
+}; 
+sum(1,2);
+
+
+//Cube Function
+const cube = (n) => {
+  return n * n * n;  
+};
+cube(3);  
+
+//Power Function
+const pow = (a,b) => {
+    return a ** b
+};
+
+//Function Hello
+const hello = () =>{
+    console.log("Hello");
+};
+
+//Implicit return in Arrow function 
+
+const mul = (a,b) => ( a * b);
+   
